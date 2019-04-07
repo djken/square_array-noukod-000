bf9508fb81fe34fdf7dbab0b113d2cff614e4903
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-07 02:24:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,13 @@
 def square_array(array)
   # your code here
   arr = []
-  array.each{|a| arr.push(a*a)}
-  return arr
+  # array.each{|a| arr.push(a*a)}
+  # return arr
   
   array.each do |a| 
     arr.push(a*a)}
   return arr
-end
+  end
 end
 
 arr = [1, 2, 3]
